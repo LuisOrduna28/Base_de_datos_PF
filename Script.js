64a08bb13b28fc8588b1d9c988a948dd7cb53658
LuisOrduna28 <luisangelorduna10@gmail.com>
2024-02-07 19:16:27 -0600

    Agrege un diseño a mi HTML y una funcion a mi NABVAR

--- Script.js
+++ Script.js
@@ -1,3 +1,26 @@
+// ======================================================= FuncionDeSecciones======================================
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(function(navLink) {
+      navLink.addEventListener("click", function(event) {
+          event.preventDefault();
+
+          const targetId = navLink.getAttribute("href").substring(1);
+
+          document.querySelectorAll(".seccion").forEach(function(section) {
+              section.classList.add("oculto");
+          });
+
+          document.getElementById(targetId).classList.remove("oculto");
+      });
+  });
+});
+
+
+// ====================================================== Prototipo =============================================
+
 class Alumno{
     constructor(nombre, apellidos, edad){
         this.nombre = nombre;
@@ -63,9 +86,9 @@ function buscarPorNombre(alumnos, nombre) {
   const alumno1 = new Alumno("Juan", "Perez", 20);
   const alumno2 = new Alumno("Maria", "Gonzalez", 21);
   const alumno3 = new Alumno("Pedro", "Lopez", 22);
-  const alumno4 = new Alumno("Pedro", "Lopez", 24);
-  const alumno5 = new Alumno("Pedro", "Lopez", 21);
-  const alumno6 = new Alumno("Pedro", "Lopez", 26);
+  const alumno4 = new Alumno("Lucia", "Aldana", 24);
+  const alumno5 = new Alumno("Antoni", "Tellez", 21);
+  const alumno6 = new Alumno("Luis", "Orduña", 26);
   
   const grupoA = new Grupo("Grupo A");
   grupoA.agregarAlumno(alumno1);
@@ -73,9 +96,9 @@ function buscarPorNombre(alumnos, nombre) {
   grupoA.agregarAlumno(alumno3);
   
   const grupoB = new Grupo("Grupo B");
-  grupoA.agregarAlumno(alumno4);
-  grupoA.agregarAlumno(alumno5);
-  grupoA.agregarAlumno(alumno6);
+  grupoB.agregarAlumno(alumno4);
+  grupoB.agregarAlumno(alumno5);
+  grupoB.agregarAlumno(alumno6);
 
   alumno1.inscribirMateria("Matematicas");
   alumno1.asignarCalificacion("Matematicas", 100);
@@ -83,9 +106,18 @@ function buscarPorNombre(alumnos, nombre) {
   alumno2.asignarCalificacion("Matematicas", 85);
   alumno3.inscribirMateria("Matematicas");
   alumno3.asignarCalificacion("Matematicas", 95);
+  alumno4.inscribirMateria("Matematicas");
+  alumno4.asignarCalificacion("Matematicas", 75);
+  alumno5.inscribirMateria("Matematicas");
+  alumno5.asignarCalificacion("Matematicas", 80);
+  alumno6.inscribirMateria("Matematicas");
+  alumno6.asignarCalificacion("Matematicas", 99);
   
   console.log("Promedio del Grupo A:", grupoA.obtenerPromedioGrupo());
+  console.log("Promedio del Grupo B:", grupoB.obtenerPromedioGrupo());
   console.log("Buscar por nombre:", buscarPorNombre(grupoA.alumnos, "Juan"));
   console.log("Buscar por apellido:", buscarPorApellido(grupoA.alumnos, "Perez"));
   console.log("Lista de alumnos ordenados por calificacion ascendente:", obtenerListaOrdenadaPorCalificacion(grupoA.alumnos));
   console.log("Lista de alumnos ordenados por calificacion descendente:", obtenerListaOrdenadaPorCalificacion(grupoA.alumnos, true));
+
+  
